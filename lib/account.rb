9ca98de5bfd7e3bb00c2ad284f75b75ff021f504
auoheheoo <chakwenc@cs.man.ac.uk>
2011-10-06 14:55:25 +0100

    second test added and passed both tests

--- lib/account.rb
+++ lib/account.rb
@@ -1,5 +1,10 @@
 class Account
+
+  def initialize starting_balance = 0
+    @balance = starting_balance
+  end
+
   def balance
-    0.0
+    @balance
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,6 +7,11 @@ describe Account do
   it "should initially have a balance of zero" do
     account = Account.new
     account.balance.should == 0.0
+  end
 
+  it "should have a balance that is equal to the starting balance" do
+    starting_balance = 100.00
+    account = Account.new starting_balance
+    account.balance.should == starting_balance
   end
 end
